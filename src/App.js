db97e7d0cec408b9702233e7337254337e39ede1
Pablo Vera <pablo.vera82@gmail.com>
2022-05-18 01:23:20 -0400

    Get news from newsapi.org

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
   // definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ca&category=${categoria}&apiKey=e615a5057c2040d4ac1e019ace84d7da`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      console.log(noticias.articles);
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
